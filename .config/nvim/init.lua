a1b238d923378e5f2887b1ed4a56bdd9f1041b07
Luke Arntz <luke@blue42.net>
2021-07-12 17:30:10 -0400

    initial commit

---
+++ .config/nvim/init.lua
@@ -0,0 +1,31 @@
+require "paq" {
+	'savq/paq-nvim'; 
+	'neovim/nvim-lspconfig';
+	'simrat39/rust-tools.nvim';
+	'rust-lang/rust.vim';
+	'nvim-treesitter/nvim-treesitter';
+	'nvim-lua/popup.nvim';
+	'nvim-lua/plenary.nvim';
+	'nvim-telescope/telescope.nvim';
+	'hrsh7th/nvim-compe';
+	'kyazdani42/nvim-web-devicons';
+	'morhetz/gruvbox';
+	'lewis6991/gitsigns.nvim';
+	
+}
+
+require('rust')
+require('complete')
+require('statusline')
+require('gitsigns').setup()
+
+
+vim.lsp.buf.formatting_sync()
+vim.o.completeopt = "menuone,noselect"
+vim.o.signcolumn = "yes"
+vim.o.updatetime = 300
+vim.o.number = true
+vim.o.cursorline = true
+vim.cmd('colorscheme gruvbox')
+
+vim.g.rustfmt_autosave = 1
---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,78 @@
+local wezterm = require 'wezterm';
+
+wezterm.on("format-window-title", function(tab, tabs, panes, config)
+  local zoomed = ""
+  if tab.active_pane.is_zoomed then
+    zoomed = "[Z] "
+  end
+
+  local index = ""
+  if #tabs > 1 then
+    index = string.format("[%d/%d] ", tab.tab_index + 1, #tabs)
+  end
+
+  return zoomed .. index .. tab.active_pane.title
+end)
+
+return {
+   automatically_reload_config = true,
+    --appearance
+    --window_background_opacity = 0.93,
+    window_padding = {
+	    left = 0,
+	    right = 0,
+	    top = 20,
+	    bottom = 0,
+    },
+    hide_tab_bar_if_only_one_tab = true,
+    font_size = 12,
+    color_scheme = "Gruvbox Dark",
+    warn_about_missing_glyphs = false,
+    -- font = wezterm.font("JetBrains Mono"),
+    font = wezterm.font_with_fallback({
+      "JetBrains Mono",
+      "Fira Code",
+    }),
+
+    colors = {
+        tab_bar = {
+            background = "#000000",
+            active_tab = {
+                bg_color = "Black",
+                fg_color = "#ebdbb2",
+                intensity = "Half",
+                underline = "None",
+                italic = false,
+                strikethrough = false,
+            },
+            inactive_tab = {
+                bg_color = "#141918",
+                fg_color = "#808080",
+            },
+            inactive_tab_hover = {
+                bg_color = "#141918",
+                fg_color = "Green",
+                italic = true,
+            },
+        },
+    },
+
+    --key mapping
+    use_dead_keys=false,
+    --leader = { key="a", mods="CTRL"},
+    keys = {
+	--{ key = "\"", mods="LEADER",     action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+	--{ key = "%",  mods="LEADER",     action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+	--{ key = "z",  mods="LEADER",     action="TogglePaneZoomState" },
+	--{ key = "H",  mods="LEADER",     action=wezterm.action{AdjustPaneSize={"Left", 5}}},
+	--{ key = "J",  mods="LEADER",     action=wezterm.action{AdjustPaneSize={"Down", 5}}},
+	--{ key = "K",  mods="LEADER",     action=wezterm.action{AdjustPaneSize={"Up", 5}}},
+	--{ key = "L",  mods="LEADER",     action=wezterm.action{AdjustPaneSize={"Right", 5}}},
+	--{ key = "v",  mods="SHIFT|CTRL", action="Paste"},
+	--{ key = "h",  mods="LEADER",     action=wezterm.action{ActivatePaneDirection="Left"}},
+	--{ key = "j",  mods="LEADER",     action=wezterm.action{ActivatePaneDirection="Down"}},
+	--{ key = "k",  mods="LEADER",     action=wezterm.action{ActivatePaneDirection="Up"}},
+	--{ key = "l",  mods="LEADER",     action=wezterm.action{ActivatePaneDirection="Right"}},
+	{ key = "F11", action="ToggleFullScreen"},
+    },
+}
